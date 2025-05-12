18db9a374b6e93989335dab817e89aed8c7281de
ZeroLinux <zerolinuxdev@gmail.com>
2025-05-12 23:35:29 +0100

    Update zerohacking.sh

--- zerohacking.sh
+++ zerohacking.sh
@@ -31,22 +31,6 @@ echo -e '\033[31;40;1m
 read -p "Numri i transaksionit: " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
 clear
-
-echo -e "\033[47;31;5m Po instalon përditësimin dhe kërkesat...\033[0m"
-sleep 5
-pkg install git -y
-pkg install python python3 -y
-pkg install pip pip3 -y
-pkg install curl -y
-apt update
-apt upgrade -y
-clear
-echo -e "\033[47;3;35m Përditësimi i plotë...\033[0m"
-sleep 3
-bash zerohacking.sh
-
-elif [[ $islem == 2 || $islem == 02 ]]; then
-clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
 cd Tools
@@ -54,7 +38,7 @@ git clone https://github.com/htr-tech/zphisher
 cd zphisher
 bash zphisher.sh
 
-elif [[ $islem == 3 || $islem == 03 ]]; then
+elif [[ $islem == 2 || $islem == 02 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -63,7 +47,7 @@ git clone https://github.com/techchipnet/CamPhish
 cd CamPhish
 bash camphish.sh
 
-elif [[ $islem == 4 || $islem == 04 ]]; then
+elif [[ $islem == 3 || $islem == 03 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -72,7 +56,7 @@ cd subscan
 read -p "Jepni nje domain p.sh (example.com): " sc
 ./subscan $sc
        
-elif [[ $islem == 5 || $islem == 05 ]]; then
+elif [[ $islem == 4 || $islem == 04 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -92,38 +76,9 @@ read -p "Jepni nje adres emaili per te bombarduar: " mail
 echo "-------------------------"
 php index.php start-bombing $mail
 
-elif [[ $islem == 6 || $islem == 06 ]]; then
+elif [[ $islem == 5 || $islem == 05 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-sleep 3
-cd Tools
-git clone https://github.com/palahsu/DDoS-Ripper.git
-cd DDoS-Ripper
-python3 DRipper.py
-echo ""
-echo -e "\033[47;3;35m Para se ta perdorni fshihni ip-n tuaj\033[0m"
-
-elif [[ $islem == 7 || $islem == 07 ]]; then
-clear
-echo "Youtube Video: https://www.youtube.com/watch?v=zgdq6ErscqY"
-python3 -m webbrowser https://www.youtube.com/watch?v=zgdq6ErscqY
-sleep 10
-echo "Prisni 10 sekonda"
-bash zerohacking.sh
-
- 
-elif [[ $islem == 8 || $islem == 08 ]]; then
-clear
-echo -e "\033[47;3;35m PO HIQEN PROGRAMET E SHKARKUARA...\033[0m"
-sleep 3 
-rm -rf Tools
-
-bash zerohacking.sh
-
-elif [[ $islem == 9 || $islem == 09 ]]; then
-clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-sleep 3
+echo -e "\033[47;3;35m Instalimi mund të marrë pak leep 3
 cd Tools
 apt update
 apt install git curl
@@ -131,7 +86,7 @@ git clone https://github.com/htr-tech/track-ip.git
 cd track-ip
 bash trackip
 
-elif [[ $islem == 10 || $islem == 010 ]]; then
+elif [[ $islem == 6 || $islem == 06 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -141,7 +96,7 @@ cd dorks-eye
 pip install -r requirements.txt
 python3 dorks-eye.py
 
-elif [[ $islem == 11 || $islem == 011 ]]; then
+elif [[ $islem == 7 || $islem == 07 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -152,7 +107,7 @@ cd hackerpro
 sudo bash install.sh
 python2 hackerpro.py
 
-elif [[ $islem == 12 || $islem == 012 ]]; then
+elif [[ $islem == 8 || $islem == 08 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -161,7 +116,7 @@ git clone https://github.com/Tuhinshubhra/RED_HAWK
 cd RED_HAWK
 php rhawk.php
 
-elif [[ $islem == 13 || $islem == 013 ]]; then
+elif [[ $islem == 9 || $islem == 09 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -173,7 +128,7 @@ pkg install git -y
 cd TigerVirus
 bash app.sh
 
-elif [[ $islem == 14 || $islem == 014 ]]; then
+elif [[ $islem == 10 || $islem == 010 ]]; then
 clear
 echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
 sleep 3
@@ -184,61 +139,6 @@ pkg install git -y
 git clone https://github.com/king-hacking/info-site.git
 cd info-site
 bash info.sh
-
-elif [[ $islem == 15 || $islem == 015 ]]; then
-clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-sleep 3
-cd Tools
-sudo apt-get update
-sudo apt-get install php
-sudo apt-get install php-curl
-git clone https://github.com/MrSqar-Ye/BadMod.git
-cd BadMod
-chmod u+x INSTALL
-chmod u+x BadMod.php
-sudo php BadMod.php
-
-elif [[ $islem == 16 || $islem == 016 ]]; then
-clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-sleep 3
-cd Tools
-git clone https://github.com/fu8uk1/facebash
-cd facebash
-bash install.sh
-chmod +x facebash.sh
-tor
-sudo ./facebash.sh
-
-elif [[ $islem == 17 || $islem == 017 ]]; then
-clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-sleep 3
-cd Tools
-pkg install git
-pkg install python2
-apt install git
-apt install python2
-git clone https://github.com/D4RK-4RMY/DARKARMY
-cd DARKARMY
-chmod +x darkarmy.py
-python2 darkarmy.py
-
-elif [[ $islem == 18 || $islem == 018 ]]; then
-clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-echo -e "\033[47;3;35m Ky tool do tju kerkoje te jeni (ROOT)\033[0m"
-sleep 3
-cd Tools
-sudo apt-get install tor
-pip3 install requests
-git clone https://github.com/FDX100/Auto_Tor_IP_changer.git
-cd Auto_Tor_IP_changer
-echo -e "\033[47;3;35m shkoni te shfletuesi juaj / ndryshoni përfaqësuesin (sock proxy) në 127.0.0.1:9050\033[0m"
-sleep 8
-python3 install.py
-aut
 
 else   
 	clear
